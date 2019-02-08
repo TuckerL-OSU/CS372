f32d2a31cf1294bf1ad20017ff2ac0abcebe99fd
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-08 15:26:56 -0500

    things are happening

--- Project1-chat/chatserve.java
+++ Project1-chat/chatserve.java
@@ -89,25 +89,28 @@ public class chatserve {
 
         try {  //Read from the socket.
 //            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
 //            CharBuffer temp = CharBuffer.allocate((500));
-            if (client.input.ready()) {
-                input = client.input.readLine();
+            while (true) {
+                if (client.input.ready()) {
+                    input = client.input.readLine();
+                    break;
 //                while (true) {
 //                    if (client.input.read(temp) != -1) {
 //                        break;
 //                    }
 //                }
+                }
             }
+
 //            input.equals(temp.array().toString());
 //            input = temp.array().toString();
 
-            clientName = input.substring(0, client.input.readLine().length());
-            String msg = input.substring(clientName.length(), input.indexOf("\0"));
-            System.out.print(clientName + "> " + msg + "\n");
-//            System.out.print(clientName + "> i typed this" + "\n");
-            System.out.print(serverName + "> ");
-            return true;
+//            clientName = input.substring(0, client.input.readLine().length());
+//            String msg = input.substring(clientName.length(), input.indexOf("\0"));
+//            System.out.print(clientName + "> " + msg + "\n");
+////            System.out.print(clientName + "> i typed this" + "\n");
+//            System.out.print(serverName + "> ");
+//            return true;
 
 //            System.out.print("input: " + client.input.readLine().length());
 
@@ -144,11 +147,11 @@ public class chatserve {
 //            System.out.println(clientName + " has disconnected");
 //            return false;
 //        }
-//        clientName = input.substring(0, 10);
-//        String msg = input.substring(clientName.length(), input.indexOf("\0"));
-//        System.out.print(clientName + "> " + msg + "\n");
-//        System.out.print(serverName + "> ");
-//        return true;
+        clientName = input.substring(0, 10);
+        String msg = input.substring(clientName.length(), input.indexOf("\0"));
+        System.out.print(clientName + "> " + msg + "\n");
+        System.out.print(serverName + "> ");
+        return true;
 //        return false;
     }
 
