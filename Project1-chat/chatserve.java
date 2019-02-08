a4eb7f3c4b275e5f040ec9ce7480057cf265dc58
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-07 19:33:00 -0500

    maybe not?

--- Project1-chat/chatserve.java
+++ Project1-chat/chatserve.java
@@ -194,12 +194,9 @@ public class chatserve {
         try (  //try-with-resources: set up socket, wait for client, set up streams.
                ServerSocket serverSocket = new ServerSocket(port);
                Socket client = serverSocket.accept();
-               BufferedReader clientSYN = new BufferedReader(new InputStreamReader(client.getInputStream()));
-               PrintWriter serverACK = new PrintWriter(client.getOutputStream(), true);
         ) {
             System.out.println(serverName + " started on port: " + port + ".\n");
-            clientName = clientSYN.readLine();
-            serverACK.println(serverName);
+
             return client;
         } catch (IOException ie) {
             System.out.println("Failed to start " + serverName + " on port: " + port + ".\n");
@@ -210,19 +207,22 @@ public class chatserve {
 
     // pass servers socket
     // need a way to send server name
-//    public static Socket estConnection(ServerSocket server) {
-//    public static Socket estConnection(Socket client) {
-//        try (
-//
-//        ) {
-//            System.out.println(clientName + " has successfully connected.\n");
-//            return client;
-//        } catch (IOException ie) {
-//            System.out.println(serverName + " failed to connect to client.\n");
-//            System.exit(1);
-//            return null;
-//        }
-//    }
+    public static Socket estConnection(ServerSocket server) {
+    public static Socket estConnection(Socket client) {
+        try (
+                PrintWriter serverACK = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader clientSYN = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        ) {
+            clientName = clientSYN.readLine();
+            serverACK.println(serverName);
+            System.out.println(clientName + " has successfully connected.\n");
+            return client;
+        } catch (IOException ie) {
+            System.out.println(serverName + " failed to connect to client.\n");
+            System.exit(1);
+            return null;
+        }
+    }
 
     public static void getName() {
         System.out.print("Enter a name for this Server (10 characters max): ");
@@ -317,8 +317,8 @@ public class chatserve {
 
         while (true) {
 //            estConnection(client);
-            client = initServer(port);
             System.out.println("Waiting for a connection...");
+            client = initServer(port);
             chat(client);
         }
     }
