0ab67f4fc8602d62e5f525f250f5351d388f4191
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-02-08 16:48:20 -0500

    I DONT KNOW

--- Project1-chat/chatserve.java
+++ Project1-chat/chatserve.java
@@ -102,7 +102,7 @@ public class chatserve {
             if (client.input.ready()) {
                 temp = client.input.readLine();
                 input = temp;
-                clientName = input.substring(0, 2);
+                clientName = input.substring(0, 10);
 //                    String msg = input.substring(10, input.indexOf('\0'));
 //                    System.out.print(clientName + "> " + msg + "\n");
                 System.out.print(clientName + "> i typed this" + "\n");
