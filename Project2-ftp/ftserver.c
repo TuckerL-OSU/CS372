dd6e79cf5b3d1c530ea878e2116f15f2542ed61d
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-03-08 10:07:58 -0500

    .

--- Project2-ftp/ftserver.c
+++ Project2-ftp/ftserver.c
@@ -114,10 +114,10 @@ int getNumFilesInDir(char** files) {
 	int i = 0;
 	if (fd) {
 		while ((dir = readdir(fd)) != NULL) {
-			if (/*!strcmp(dir->d_name, ".") == 0 ||*/ !strcmp(dir->d_name, "..") == 0) {
+			//if (/*!strcmp(dir->d_name, ".") == 0 ||*/ !strcmp(dir->d_name, "..") == 0) {
 				strcpy(files[i], dir->d_name);
 				i++;
-			}
+			//}
 		}
 		closedir(fd);
 	}
